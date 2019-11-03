e7fa2621d6bf8ee03ff73839143f76a384f5fc4d
Logan Sysling <syslinglj@vcu.edu>
2019-11-03 18:04:09 -0500

    commit take 2

--- ATM/Source.cpp
+++ ATM/Source.cpp
@@ -6,19 +6,23 @@ void getBills(int& one, int& five, int& ten, int& twenty, int& fifty, int& amoun
 
 int main()
 {
-	int ones, fives, tens, twenties, fifties;
+	int ones = 0 , fives = 0, tens = 0, twenties = 0, fifties = 0;
 	int amount = 0;
-	char a = 'Y';
-	while (a == 'Y' || a == 'y')
+	char a = '\0';
+	do
 	{
 		int ones = 0, fives = 0, tens = 0, twenties = 0, fifties = 0;
-		getBills(ones, fives, tens, twenties, fifties, amount); 
-		if (amount < 1 || amount > 300)
+		getBills(ones, fives, tens, twenties, fifties, amount); //calls getBills() for the amount of bills from user request
+		if (amount < 1 || amount > 300) // checks to make sure amount is valid
 		{
+			if (amount == 0) // ends if amount is zero
+				break;
 			cout << "You have requested too much, or entered a negative value." << endl;
 			cout << "Please enter a new value." << endl;
+			cout << "Would you like to try again? Y:N" << endl;
+			cin >> a;
 		}
-		else 
+		else // continues if amount is valid and prints out amount of bills if they are more than 0
 		{
 			cout << "You wanted to withdraw: " << amount << endl;
 			if (fifties > 0)
@@ -32,13 +36,14 @@ int main()
 			if (ones > 0)
 				cout << "You have received " << ones << " ones." << endl;
 
-			//cout << "Your change will be " << fifties << " Fifties, " << twenties << " Twenties, " << tens << " Tens, " << fives << " Fives, " << ones << " Ones." << endl;
 			cout << "Would you like to withdraw more? Y:N" << endl;
 			cin >> a;
 		}
-	}
+	} while (a == 'Y' || a == 'y'); 
 	cout << "Goodbye!" << endl;
 }
+// takes amount from getAmount() and counts bills 
+
 
 void getBills(int& one, int& five, int& ten, int& twenty, int& fifty, int& amount)
 {
@@ -72,10 +77,10 @@ void getBills(int& one, int& five, int& ten, int& twenty, int& fifty, int& amoun
 	}
 }
 
-int getAmount()
+int getAmount()// requests input from user
 {
 	int value;
-	cout << "Please enter a withdrawal amount --> ";
+	cout << "Please enter a withdrawal amount, or 0 to exit --> ";
 	cin >> value;
 	return value;
 }
